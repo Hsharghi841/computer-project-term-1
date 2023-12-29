491e4c5e17827b14d2d39ec59d12fa7d5fc06b69
mahdiyaar <m.hosein.assarnia@gmail.com>
2023-12-29 10:58:42 +0330

    add jadval

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-/*#include <allegro5/allegro.h>
+#include <allegro5/allegro.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_ttf.h>*/
+#include <allegro5/allegro_ttf.h>
 
 #include <stdio.h>
 #include <math.h>
@@ -27,8 +27,9 @@ int abs(int x);
 void sortObject(int id[], int idnum, int boardSize);
 int startSettingBoard(int boardSize);
 int putOnboard(int id, int width, int length);// putOnboard tries to put an object on board if can return 1 else 0 :
-//void initscrean();
+void initscrean(int n);
 void initwall(int n);
+void show_board(int n);
 
 
 // structures for objects (mice, cats, dogs)
@@ -52,59 +53,28 @@ int wall[31][31];
 
 
 int main(){
+    int n;
 	srand(time(0));
-    
-    
-    
-    
-    // int n = 15;
-    // int a[4] = {27, 28, 29, 30};
-	// sortObject(a, 4, n);
+    scanf("%d",&n);
+    initwall(n);
+    startSettingBoard(n);
+    initscrean(n);
+ }
 
-    // for (size_t i = 0; i < n; i++)
-    // {
-    //     for (size_t j = 0; j < n; j++)
-    //     {
-    //         printf("%d", !!board[i][j][1]);
-    //     }
-    //     printf("\n");
-    // }
-    
-
-
-
-//    int c;
-//	srand(time(0));
-////   initscrean();
-//    scanf("%d",&c);
-//    initwall(c);
-//    for (int i = 0; i < c; i++)
-//    {
-//        for (int j = 0; j < c; j++)
-//        {
-//
-//            printf("%d",wall[i][j]);
-//
-//        }
-//        printf("\n");
-//    }
-}
-
-
-/*void initscrean()
+void initscrean(int n)
 {
-	   // ALLEGRO_DISPLAY *display;
-  //  ALLEGRO_BITMAP *cursor;
+	// ALLEGRO_DISPLAY *display;
+    //  ALLEGRO_BITMAP *cursor;
     ALLEGRO_MOUSE_STATE msestate;
-  //  ALLEGRO_KEYBOARD_STATE kbdstate;
+    //  ALLEGRO_KEYBOARD_STATE kbdstate;
     al_init();
     al_init_font_addon();
     al_init_primitives_addon();
     al_install_mouse();
-    al_install_keyboard();
+    //al_install_keyboard();
     al_init_image_addon();
     //init_platform_specific();
-    ALLEGRO_DISPLAY * display = al_create_display(800 ,600);
+    ALLEGRO_DISPLAY * display = al_create_display(1280 ,720);
     ALLEGRO_FONT * font = al_create_builtin_font();
 	al_hide_mouse_cursor(display);
     ALLEGRO_BITMAP *cursor=al_load_bitmap("cursor.tga");
@@ -113,31 +83,27 @@ int main(){
     }
     int i=1,j=0,mode=1;
     while(1){
-    	
-    	j=(j+1)%400;
+    	al_clear_to_color(al_map_rgb(0, 0, 0));
     	al_get_mouse_state(&msestate);
+    	show_board(n);
     	al_draw_bitmap(cursor,msestate.x,msestate.y,0);
-    	//al_draw_text(font, al_map_rgb(255, 255, 255),3, 300, 400, "Hello World");
-    	if(al_mouse_button_down(&msestate,i))
-    		mode*=-1;
-        if (mode==1)
-        {
-            al_clear_to_color(al_map_rgb(0, 0, 0));
-            al_draw_bitmap(cursor,msestate.x,msestate.y,0);
-            al_draw_text(font, al_map_rgb(255, 255, 255),j, 300, 400, "Hello World");
-            al_rest(0.05);
-        }
-        else
-        {
-            al_clear_to_color(al_map_rgb(0, 255, 255));
-            al_draw_bitmap(cursor,msestate.x,msestate.y,0);
-            al_draw_text(font, al_map_rgb(255, 50, 150),j, 300, 400, "shargi bebin");
-			al_rest(0.05);
-        }
-        
+        al_draw_bitmap(cursor,msestate.x,msestate.y,0);
     	al_flip_display();
-    //al_rest(10.0);
-}*/
+        al_rest(1.0/90);
+	}
+}
+
+void show_board(int n)
+{
+    float x,y;
+    x=y=700/n;
+    for (int i = 10; i < 710; i+=x)
+        for (int j = 10; j < 710; j+=y){
+            al_draw_filled_rectangle(i,j,i+x,j+y, al_map_rgb(150,150,50));
+            al_draw_rectangle(i,j,i+x,j+y, al_map_rgb(0,150,50),2);
+        }
+    
+}
 
 void initwall(int n)
 {
@@ -184,8 +150,6 @@ void initwall(int n)
         }
     }
 }
-
-
 
 void initAnimals(){
 
